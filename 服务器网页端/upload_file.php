fe57bf68f04de57ccb51ea19be42f416446f2a7a
547414 <zbshr@foxmail.com>
2020-10-24 15:01:44 +0800

    4.0.0.1006
    1.服务器网页端增加验证码，防止恶意机器攻击
    2.服务器网页端更改随机数+时间戳的形式命名上传文件，进一步增加文件的安全

--- 服务器网页端/upload_file.php
+++ 服务器网页端/upload_file.php
@@ -10,21 +10,26 @@ if ($_FILES["file"]["type"] == "text/plain") {
         echo "文件: " . $_FILES["file"]["name"] . "<br />";
         echo "类型: " . $_FILES["file"]["type"] . "<br />";
         echo "大小: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-        // 文件检测
+        // 文件检测,这里的upload最好改一下，改成随机的，比如dakfjjkgfryyai3whja之类的，可以很大程度上防止盗取题库文件
         if (file_exists("upload/" . $_FILES["file"]["name"])) {
             echo $_FILES["file"]["name"] . " 文件已经上传过. ";
         } else {
             // 创建文件夹
             is_dir('upload') || mkdir("upload", 0777, true);
             // 执行存储
-			$date = date('Y-m-d H:i:s');
-			$date = str_replace(' ', '', $date);
-			$date = str_replace('-', '', $date);
-			$date = str_replace(':', '', $date);
-            move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" .$date.".txt");
-            echo "微信小程序导入ID: " . $date;
+			//$date = date('Y-m-d H:i:s');
+			//$date = str_replace(' ', '', $date);
+			//$date = str_replace('-', '', $date);
+			//$date = str_replace(':', '', $date);
+			$date=microtime(true);//获取时间戳
+            $date=str_replace('.','',$date);
+            $str = rand(10, 10000);
+            $temp=substr($date,strlen($date)/2-2,strlen($date)/2+1);
+            $temp=$str.$temp;
+            move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" .$temp.".txt");
+            echo "微信小程序导入ID: " . $temp;
         }
     }
 } else {
-    echo "上传文件类型不支持";
+    echo "上传文件类型不支持哦";
 }
